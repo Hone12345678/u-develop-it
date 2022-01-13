0b46023a3a8c14b1933a0089121dcea80beeab7a
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-01-12 21:23:36 -0700

    mySQL is now working with the server

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config();
 
 // Express middleware
 
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: process.env.MYSQL_PASSWORD,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+      console.log(rows);
   });
 
 // Default response for any other request (Not Found)
